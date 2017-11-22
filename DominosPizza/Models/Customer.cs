141e84cbc71263cc89afc56a55e7b8f58205060c
e1ement <kondrov.v@gmail.com>
2017-11-23 00:17:10 +0300

    add models

--- DominosPizza/Models/Customer.cs
+++ DominosPizza/Models/Customer.cs
@@ -7,47 +7,21 @@ namespace DominosPizza.Models
 {
     public class Customer
     {
-        private int customerId;
-        private string customerName;
-        private string customerPatronymicName;
-        private string customerSurName;
-        private DateTime customerBirthDate;
-        private DateTime customerCreateDate;
+        private int CustomerId { get; set; }
+        private string CustomerFirstName { get; set; }
+        private string CustomerLastName { get; set; }
+        private string CustomerPatronymic { get; set; }
+        private DateTime CustomerBirthDate { get; set; }
+        private Boolean CustomerSex { get; set; }
+        private ICollection<Contacts> ContactsId { get; set; }
 
-        public void NewCustomer(string Name, string PatronymicName, string SurName, DateTime BirthDate)
-        {
-            customerName = Name;
-            customerPatronymicName = PatronymicName;
-            customerSurName = SurName;
-            customerBirthDate = BirthDate;
-            customerCreateDate = DateTime.Now;
-        }
-
-        public int CustomerId
-        {
-            get
-            {
-                return customerId;
-            }
-        }
-
-        public DateTime CustomerCreateDate
-        {
-            get
-            {
-                return customerCreateDate;
-            }
-        }
-        public DateTime CustomerBirthDate
-        {
-            get
-            {
-                return customerBirthDate;
-            }
-        }
-        public string CustomerFullName()
-        {
-            return $"{customerName} {customerPatronymicName} {customerSurName}";
-        }
+        //public void NewCustomer(string Name, string PatronymicName, string SurName, DateTime BirthDate)
+        //{
+        //    customerName = Name;
+        //    customerPatronymicName = PatronymicName;
+        //    customerSurName = SurName;
+        //    customerBirthDate = BirthDate;
+        //    customerCreateDate = DateTime.Now;
+        //}
     }
 }
